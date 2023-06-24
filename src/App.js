24d208f7e5baf852f8f49e9ef2681752a54c3b23
pyojeyn <jeynpyo@gmail.com>
2023-06-24 15:02:57 +0900

    http 오류 처리하기 , 요청에 useEffect 사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,24 +8,58 @@ function App() {
 
   const [isLoading, setIsLoading] = useState(false);
 
-async function fetchMoviesHandler() {
-  setIsLoading(true);
-    
-  const response = await fetch('https://swapi.dev/api/films');
-  const data = await response.json();
+  const [error, setError] = useState(null);
 
-  const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
-      setIsLoading(false);
-  }
   
+
+  const  fetchMoviesHandler = useCallback(async () =>{
+    setIsLoading(true);
+    setError(null);
+    try{
+      const response = await fetch('https://swapi.dev/api/films/');
+      
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+    
+      const transformedMovies = data.results.map(movieData => {
+            return {
+              id: movieData.episode_id,
+              title: movieData.title,
+              openingText: movieData.opening_crawl,
+              releaseDate: movieData.release_date
+            };
+          });
+          setMovies(transformedMovies);     
+    }catch(error){
+      setError(error.message);
+
+    }
+    setIsLoading(false);
+  },[]);
+
+    
+  useEffect(() => {
+    console.log('useEffect 실행')
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  
+  
+  let content = <p>Found no movies</p>
+
+  if(movies.length > 0){
+    content = <MoviesList movies={movies} />
+  }
+
+  if(error){
+    content = <p>{error}</p>
+  }
+
+  if(isLoading){
+    content = <p>Loading...</p>
+  }
 
   return (
     <React.Fragment>
@@ -33,9 +67,7 @@ async function fetchMoviesHandler() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
